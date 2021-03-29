13f2ae2515a12cee7ff8af6ea37757b5a1c29f43
Nicolasdha <sankp001@gmail.com>
2021-03-29 17:54:09 -0600

    Original git commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      {/*Home*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDlXuaUTnb82U7GOOgCBn32IXni0BkVloE",
+  authDomain: "ndurikhaecommclone.firebaseapp.com",
+  projectId: "ndurikhaecommclone",
+  storageBucket: "ndurikhaecommclone.appspot.com",
+  messagingSenderId: "18949854764",
+  appId: "1:18949854764:web:fe7f2a16b9fc7dc9a5a389",
+  measurementId: "G-CXGZVWGWYE",
+};
